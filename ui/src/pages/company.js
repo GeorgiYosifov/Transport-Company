b78e64afc88f7915d40155d68cc8b120b66f6439
svetlimladenov <svetlimladenov@gmail.com>
2022-01-10 23:17:11 +0200

    brat

--- ui/src/pages/company.js
+++ ui/src/pages/company.js
@@ -11,6 +11,7 @@ import Select from '../components/select';
 import FormCol from '../components/form-col';
 import Button from '../components/button';
 import FormRow from '../components/form-row';
+import Input from '../components/input';
 
 const VEHICLE_OPTIONS = ['Bus', 'TIR', 'Lorry'];
 
@@ -23,6 +24,7 @@ const Company = () => {
   });
 
   const [vehicleType, setVehicleType] = useState('Bus');
+  const [employee, setEmployee] = useState({});
 
   useEffect(() => {
     getCompany(id).then((data) => {
@@ -47,7 +49,6 @@ const Company = () => {
         { id: (company?.vehicles?.length || 0) + 1, type: vehicleType },
       ],
     };
-    console.log(updatedCompany);
     await updateCompany(updatedCompany);
     setCompany(updatedCompany);
   };
@@ -58,6 +59,18 @@ const Company = () => {
       employees: company.employees.filter(({ id: oldId }) => oldId !== id),
     };
 
+    await updateCompany(updatedCompany);
+    setCompany(updatedCompany);
+  };
+
+  const handleAddEmployee = async () => {
+    const updatedCompany = {
+      ...company,
+      employees: [
+        ...(company.employees || []),
+        { id: (company?.employees?.length || 0) + 1, ...employee },
+      ],
+    };
     await updateCompany(updatedCompany);
     setCompany(updatedCompany);
   };
@@ -78,7 +91,7 @@ const Company = () => {
         </section>
         <div className="grid grid-rows-2 grid-flow-col gap-1 shadow-lg p-4">
           <FormRow>
-            <FormCol className={'md:w-1/2'}>
+            <FormCol className={'md:w-1/3'}>
               <StyledLabel>Vehicle Type</StyledLabel>
               <Select
                 onChange={(value, name) => setVehicleType(value)}
@@ -88,6 +101,29 @@ const Company = () => {
                 }))}
               />
               <Button onClick={handleAddVehicle}>Add</Button>
+            </FormCol>
+          </FormRow>
+          <FormRow>
+            <FormCol className="md:w-1/3">
+              <StyledLabel htmlFor="grid-name">Name</StyledLabel>
+              <Input
+                id="grid-name"
+                type="text"
+                onChange={(name, value) =>
+                  setEmployee((employee) => ({ ...employee, name: value }))
+                }
+              />
+              <Button onClick={() => handleAddEmployee()}>Add Employee</Button>
+            </FormCol>
+            <FormCol className="md:w-1/3">
+              <StyledLabel htmlFor="grid-salary">Salary</StyledLabel>
+              <Input
+                id="grid-salary"
+                type="text"
+                onChange={(name, value) =>
+                  setEmployee((employee) => ({ ...employee, salary: value }))
+                }
+              />
             </FormCol>
           </FormRow>
         </div>
